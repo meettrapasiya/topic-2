39d046743a42ce5d8cf4ad1fb9f305d35a356c83
meettrapasiya <trapasiyameet78@gmail.com>
2024-06-20 12:52:27 +0530

    Add files via upload

---
+++ task 2.2   2.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+main()
+
+{
+	float l,area;
+	
+	printf("Enter Length of rectangle =");
+	scanf("%f",&l);
+	
+	area = 4.12 * l *l;
+	printf("Area of rectengle = %.2f",area);
+}
